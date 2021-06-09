a832fcbd42316fbc6c8fe7d63545456e798908aa
LucasCambon <Lucambon08@gmail.com>
2021-06-09 00:49:21 -0300

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 app.use('/public/img', express.static(__dirname +'/public/img'));
 app.use('/public/css', express.static(__dirname + '/public/css'));
+app.use('/views', express.static(__dirname + '/views'));
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
 })
@@ -11,15 +12,5 @@ app.listen(process.env.PORT || 3000, function() {
 
 app.get("/", (req, res) => {
     let htmlPath= path.join(__dirname, "/views/home.html")
-    res.sendFile(htmlPath)
-});
-
-app.get("/", (req, res) => {
-    let htmlPath= path.join(__dirname, "/views/register.html")
-    res.sendFile(htmlPath)
-});
-
-app.get("/", (req, res) => {
-    let htmlPath= path.join(__dirname, "/views/login.html")
     res.sendFile(htmlPath)
 });
