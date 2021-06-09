dbe4bfe8344ae0c262657b6110fa324442964648
LucasCambon <Lucambon08@gmail.com>
2021-06-09 00:50:53 -0300

    fixes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const path = require('path');
 app.use('/public/img', express.static(__dirname +'/public/img'));
 app.use('/public/css', express.static(__dirname + '/public/css'));
 app.use('/views', express.static(__dirname + '/views'));
+
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
 })
-
 
 app.get("/", (req, res) => {
     let htmlPath= path.join(__dirname, "/views/home.html")
